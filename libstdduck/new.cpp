525d197c2a24cf6e801ef0eb47f2ffce4a17cb49
wys <wangyisong1996@126.com>
2018-07-09 10:44:30 +0800

    Support new handler

--- libstdduck/new.cpp
+++ libstdduck/new.cpp
@@ -1,11 +1,22 @@
 #include <cstdlib>
+#include <new>
+
+static std::new_handler the_new_handler = 0;
 
 void * operator new (std::size_t size) {
-	return malloc(size);
+	void *ret = malloc(size);
+	if (!ret) {
+		the_new_handler();
+	}
+	return ret;
 }
 
 void * operator new[] (std::size_t size) {
-	return malloc(size);
+	void *ret = malloc(size);
+	if (!ret) {
+		the_new_handler();
+	}
+	return ret;
 }
 
 void operator delete (void *p) {
@@ -16,9 +27,19 @@ void operator delete[] (void *p) {
 	return free(p);
 }
 
-// Make it a page fault :)
 namespace std {
+	// Make it a page fault :)
 	extern "C" void __throw_bad_alloc() {
 		*(int *) 0 = 0;
+	}
+	
+	new_handler set_new_handler(new_handler new_p) {
+		new_handler ret = the_new_handler;
+		the_new_handler = new_p;
+		return ret;
+	}
+	
+	extern "C" new_handler get_new_handler() {
+		return the_new_handler;
 	}
 }
